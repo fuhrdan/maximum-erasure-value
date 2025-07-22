70b4535785348470d482424b3b901e77e83fc336
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-07-22 11:02:47 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//*****************************************************************************
+//** 1695. Maximum Erasure Value                                    leetcode **
+//*****************************************************************************
+
+int maximumUniqueSubarray(int* nums, int numsSize) {
+    int fried[100001] = {0};
+    int left = 0;
+    int right = 0;
+    int hashbrown = 0;
+    int order = 0;
+
+    while(right < numsSize)
+    {
+        int val = nums[right];
+        while(fried[val])
+        {
+            fried[nums[left]] = 0;
+            hashbrown -= nums[left];
+            left++;
+        }
+
+        fried[val] = 1;
+        hashbrown += val;
+        if(order < hashbrown)
+        {
+            order = hashbrown;
+        }
+        right++;
+    }
+
+    return order;
+}
